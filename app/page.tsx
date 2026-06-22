518c50b8130af2584115e0615c48c9a5237cc3d8
Rodrigo Carvalho <rodrigocarvalhoshen@gmail.com>
2026-06-22 06:59:04 -0300

    changing the README.md and creating the folder section in components

--- app/page.tsx
+++ app/page.tsx
@@ -18,15 +18,16 @@ export default function Home() {
         >
           <h1 className="font-bold text-2xl">Bem vindo ao<br/>Check Design</h1>
           <h2 className="font-semibold text-xl opacity-50">O aplicativo de análise de <br/> screenshots de designers UX</h2>
-          <p className="font-normal text-[0.50rem]">
+          <p className="font-normal text-[0.75rem]">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including.
           </p>
           <button 
-            className="flex px-5 py-2 w-26 h-9 justify-center items-center bg-secundary-color text-black-color text-[0.5rem]"
+            className="flex px-5 py-2 mt-5 w-40 h-12 justify-center items-center bg-secundary-color text-black-color text-[0.8rem]"
           >
             See More
           </button>
         </section>
+        
       </main>
         
       </motion.div>
